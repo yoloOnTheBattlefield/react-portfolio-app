46a48de6aeab251f8582f97fa8321315b0afc7c8
Cristi Florea <cristianflorea1110@yahoo.com>
2017-05-01 14:12:16 +0100

    updates

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 
 const ContentSection = styled.div`
   margin-top: 5%;
-  height: 100%;
-  min-height: 100%;
   @media (max-width: 800px){
     display: flex;
     flex-direction: column;
